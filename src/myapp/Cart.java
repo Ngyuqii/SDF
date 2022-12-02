feeb146fd188aee7bfbf417d1d3456db0eb7ce58
ngyuqi <ngyuqi89@gmail.com>
2022-12-02 10:44:44 +0800

    Commit

---
+++ src/myapp/Cart.java
@@ -0,0 +1,21 @@
+package myapp;
+
+import java.io.Console;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Cart {
+    public static void main(String[] args) {
+        List<String> CartItems = new LinkedList<String>();
+        Console cons = System.console();
+
+        System.out.println("Welcome to your shopping cart.");
+        String UserInput = cons.readLine (">")
+        UserInput = UserInput.trim();
+        String input = line.split("");
+
+       switch (input)
+
+    }
+
+}
